7887c58bc67c0cc942a501bb3d3718eb1061350b
ctaylor133 <44619551+ctaylor133@users.noreply.github.com>
2022-03-29 17:50:52 -0700

    adding header

--- CheckAllSErvices.ps1
+++ CheckAllSErvices.ps1
@@ -1,3 +1,19 @@
+####CSS Header####
+$Header = @"
+<style>
+TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;}
+TH {border-width: 1px; padding: 3px; border-style: solid; border-color: black; background-color: #6495ED;}
+TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+</style>
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js" integrity="sha512-qzgd5cYSZcosqpzpn7zF2ZId8f/8CHmFKZ8j7mU4OUXTNRd5g+ZHBPsgKEwoqxCtdQvExE5LprwwPAgoicguNg==" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+$(function() {
+        $(document).find("table").tablesorter();
+});
+</script>
+"@
+
 $OasisServers = Get-Content $ENV:UserProfile\Desktop\server-list.txt
 
 foreach($Server in $OasisServers){
