c907a599613161e5f36a1f857c62b87a0cce4932
ctaylor133 <44619551+ctaylor133@users.noreply.github.com>
2022-06-21 14:35:51 -0700

    Add TLS checking to script

--- DHCPinfo.ps1
+++ DHCPinfo.ps1
@@ -1,10 +1,23 @@
 ## Script to gather DHCP info from a remote server and output it to csv
 ## To do: Create a variable for -ScopeID
+## To do: Create option for Poller DHCP servers
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+$Header = @"
+<style>
+TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;}
+TH {border-width: 1px; padding: 3px; border-style: solid; border-color: black; background-color: #6495ED;}
+TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+</style>
+"@
 
 ## Removing the slashes from logon server output
 $pattern = '[\\]'
 $dcname = $env:LOGONSERVER
 $string = $dcname -replace $pattern, ''
 
-Invoke-Command -ComputerName $string -ScriptBlock {Get-DhcpServerv4Scope -Computername $using:string} | Out-File -FilePath $ENV:UserProfile\Desktop\dhcpinfo.csv
-Invoke-Command -ComputerName $string -ScriptBlock {Get-DhcpServerv4OptionValue -Computername $using:string -ScopeID "10.139.123.0"} | Out-File -Append -FilePath $ENV:UserProfile\Desktop\dhcpinfo.csv
+
+
+Invoke-Command -ComputerName $string -ScriptBlock {Get-DhcpServerv4Scope -Computername $using:string} | Select @{Expression={$_.PSComputerName};Label="Server Name"}, @{Expression={$_.SubnetMask};Label="SubnetMask"}, @{Expression={$_.ScopeID};Label="ScopeID"}, @{Expression={$_.LeaseDuration};Label="Lease Time"} | ConvertTo-Html -Head $Header | Out-File -FilePath $ENV:UserProfile\Desktop\dhcpinfo.html
+Invoke-Command -ComputerName $string -ScriptBlock {Get-DhcpServerv4OptionValue -Computername $using:string -ScopeID "10.139.123.0"} | Select @{Expression={$_.Name};Label="DNS Name"}, @{Expression={$_.Value};Label="DNS Servers"} | ConvertTo-Html -Head $Header | Out-File -Append -FilePath $ENV:UserProfile\Desktop\dhcpinfo.html
+Invoke-Command -ComputerName $string {[Net.ServicePointManager]::SecurityProtocol} | select @{Expression={$_.PSComputername};Label="Server"}, @{Expression={$_.value};Label="TLS"} | ConvertTo-Html -Head $Header | Out-File -Append -FilePath $ENV:UserProfile\Desktop\dhcpinfo.html
