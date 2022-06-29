7c07ae39535637a68260975a7ebc8fd1e65484fe
ctaylor133 <44619551+ctaylor133@users.noreply.github.com>
2022-06-29 11:14:38 -0700

    added more instructions and notes

--- dhcp_bad_leases.ps1
+++ dhcp_bad_leases.ps1
@@ -1,5 +1,7 @@
 ## Short script to grab the bad leases from a DHCP server, delete them, wait 30 seconds and print out how many bad leases there are.
 ## TO do -- make varriable for fetching ScopeID(s)
+## Run the below command by itself and grab the ScopID and replace "IP" in line 5
+### Get-DhcpServerv4Scope
 
 $scope = "IP"
 
